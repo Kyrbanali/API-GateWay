e549e8224c2e7cc65067af9e5074a55519d4f430
Kyrbanali <arslanaliev.ruby@gmail.com>
2025-06-24 14:28:20 +0300

    добавил SQL операции для user

---
+++ internal/storage/user.go
@@ -0,0 +1,34 @@
+package storage
+
+import (
+	"context"
+
+	"github.com/Kyrbanali/API-GateWay/internal/models"
+)
+
+func InsertUser(ctx context.Context, user models.User) error {
+	_, err := Conn.Exec(ctx, `
+		INSERT INTO USERS (id, name, age) VALUES ($1, $2, $3)
+	`, user.ID, user.Name, user.Age)
+	return err
+}
+
+func GetUserById(ctx context.Context, id string) (models.User, error) {
+	var user models.User
+	err := Conn.QueryRow(ctx, `
+		SELECT id, name, age FROM users WHERE id = $1
+	`, id).Scan(&user.ID, &user.Name, &user.Age)
+	return user, err
+}
+
+func UpdateUser(ctx context.Context, user models.User) error {
+	_, err := Conn.Exec(ctx, `
+		UPDATE user SET name = $1, age = $2 WHERE id = $3
+	`, user.Name, user.Age, user.ID)
+	return err
+}
+
+func DeleteUser(ctx context.Context, id string) error {
+	_, err := Conn.Exec(ctx, `DELETE FROM users WHERE id = $1`, id)
+	return err
+}
