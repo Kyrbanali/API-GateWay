d3011bea3afc82aad3e37cb2102cff845e268e82
Kyrbanali <arslanaliev.ruby@gmail.com>
2025-06-29 15:09:49 +0300

    добавил проверки на подключение и пинг ДБ

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,10 +2,20 @@ package storage
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func GetConnect(connStr string) (*pgxpool.Pool, error) {
-	return pgxpool.New(context.Background(), connStr)
+	pool, err := pgxpool.New(context.Background(), connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect pgx pool: %w", err)
+	}
+
+	if err := pool.Ping(context.Background()); err != nil {
+		return nil, fmt.Errorf("failed to ping to DB: %w", err)
+	}
+
+	return pool, nil
 }
