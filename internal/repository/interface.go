545956f0c5a8ce71163b657f2d144a4e8c8ffd9e
Kyrbanali <arslanaliev.ruby@gmail.com>
2025-07-03 00:46:38 +0300

    removed func new() from interfaces

--- internal/repository/interface.go
+++ internal/repository/interface.go
@@ -4,11 +4,9 @@ import (
 	"context"
 
 	"github.com/Kyrbanali/API-GateWay/internal/models"
-	// "github.com/jackc/pgx/v5/pgxpool"
 )
 
 type UserProvider interface {
-	// New(conn *pgxpool.Pool) *UserRepo
 	CreateUser(ctx context.Context, user models.UserDTO) (string, error)
 	GetUserByID(ctx context.Context, id string) (*models.UserDTO, error)
 	GetAllUsers(ctx context.Context) ([]models.UserDTO, error)
--- internal/usecase/interface.go
+++ internal/usecase/interface.go
@@ -4,11 +4,9 @@ import (
 	"context"
 
 	"github.com/Kyrbanali/API-GateWay/internal/models"
-	// "github.com/Kyrbanali/API-GateWay/internal/repository"
 )
 
 type UserProvider interface {
-	// New(userRepo *repository.UserRepo) *UseCase
 	CreateUser(ctx context.Context, user models.UserDTO) (string, error)
 	GetUserByID(ctx context.Context, id string) (*models.UserDTO, error)
 	GetAllUsers(ctx context.Context) ([]models.UserDTO, error)
